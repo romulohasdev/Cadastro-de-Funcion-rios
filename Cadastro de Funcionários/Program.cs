4598a52d595a61566af43898c18f6fd760b9fcde
Rômulo Henrique <henriqueromulo1997@gmail.com>
2023-10-20 12:01:35 -0300

    Criação do menu principal do projeto

--- Cadastro de Funcionários/Program.cs
+++ Cadastro de Funcionários/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Menu
+{
+    public void MenuPrincipal()
+    {
+        int op = 1;
+        while (op != 0)
+        {
+            Console.Clear();
+            Console.WriteLine("Bem-vindo, Escolha uma das seguintes opções:");
+            Console.WriteLine("1 - Cadastrar Novo Funcionário");
+            Console.WriteLine("2 - Listar Funcionários");
+            Console.WriteLine("0 - Sair\n");
+            op = int.Parse(Console.ReadLine());
+
+            if (op == 0)
+            {
+                break;
+            }
+            else if (op == 1)
+            {
+                Console.Clear();
+                Console.WriteLine("\nCadastrando Funcionário");
+                Console.ReadKey();
+                // Colocar sistema de Cadastro de Funcionário que fique salvo em uma lista
+            }
+            else if (op == 2)
+            {
+                Console.Clear();
+                Console.WriteLine("\nListando Funcionários");
+                Console.ReadKey();
+                // Acessar lista que tenha funcionários cadastrados
+            }
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Menu menu = new Menu();
+        menu.MenuPrincipal();
+    }
+}
